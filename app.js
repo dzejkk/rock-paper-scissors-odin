a4a492f6544bf7d547c8b9292344927739e337e0
kontrajakub2@gmail.com <visitero@MacBook-Air-uzivatela-visitero.local>
2024-11-17 20:26:23 +0100

    get round

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //computer choice
 
 function getComputerChoice() {
-  const options = ["papier", "kamen", "noznice"]; 
+  const options = ["papier", "kamen", "noznice"];
 
-  let randomIndex = Math.floor(Math.random() * options.length); 
+  let randomIndex = Math.floor(Math.random() * options.length);
 
   return options[randomIndex];
 }
@@ -15,7 +15,26 @@ function hasPlayerWonTheRound(player, computer) {
     (player === "kamen" && computer === "noznice") ||
     (player === "noznice" && computer === "papier") ||
     (player === "papier" && computer === "kamen")
-  ) 
+  );
 }
 
-//get round result 
+//get round result
+
+let playerScore = 0;
+let computerScore = 0;
+
+const getRoundResult = (playerOption) => {
+  const computerOption = getComputerChoice();
+
+  const round = hasPlayerWonTheRound(playerOption, computerOption);
+
+  if (round === true) {
+    playerScore++;
+    return "player won";
+  } else {
+    computerScore++;
+    return "computer won";
+  }
+};
+
+console.log(getRoundResult("noznice"));
