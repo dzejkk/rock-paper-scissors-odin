e72237e378bedaea4193c543b256dec156d2233f
kontrajakub2@gmail.com <visitero@MacBook-Air-uzivatela-visitero.local>
2024-12-09 20:15:12 +0100

    added button funcionality

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+const scoreDisplay = document.getElementById("score-display");
+
+const rockBtn = document.getElementById("player-choice-button-rock");
+const paperBtn = document.getElementById("player-choice-button-paper");
+const scissorsBtn = document.getElementById("player-choice-button-scissors");
+
+
+
+
+
+
 //computer choice
 
 function getComputerChoice() {
   const options = ["papier", "kamen", "noznice"];
-
+  
   let randomIndex = Math.floor(Math.random() * options.length);
-
+  
   return options[randomIndex];
 }
 
@@ -25,9 +36,8 @@ let computerScore = 0;
 
 const getRoundResult = (playerOption) => {
   const computerOption = getComputerChoice();
-
   
-
+  
   if (hasPlayerWonTheRound(playerOption,computerOption)) {
     playerScore++;
     return "player won";
@@ -40,4 +50,24 @@ const getRoundResult = (playerOption) => {
   }
 };
 
-console.log(getRoundResult("paper"));
+
+// funkcia ktora ukaze vysledok
+
+function showResult (playerOption) {
+  
+  scoreDisplay.innerText = getRoundResult(playerOption);
+  
+}
+
+
+rockBtn.addEventListener("click", function () {
+  showResult("kamen")
+})
+
+paperBtn.addEventListener("click", function() {
+  showResult("papier")
+})
+
+scissorsBtn.addEventListener("click", function() {
+  showResult("noznice")
+})
